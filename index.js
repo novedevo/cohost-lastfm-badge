1aab0612e8bbebc7d8e7e1033b3fc857dba9b091
Devon Sawatsky <novedevo@gmail.com>
2023-05-18 10:44:27 -0700

    make it just redirect to the lfm image

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ async function getAlbums(username) {
 	const params = new URLSearchParams({
 		method: "user.getrecenttracks",
 		user: username,
-		limit: 1,
+		limit: "1",
 		api_key: "0a828de6701971f3766542996b54c24b",
 		format: "json",
 	});
 	url.search = params.toString();
-
 	const json = await fetch(url).then((res) => res.json());
-	console.log(json);
-	const tracks = json.recenttracks.track;
-
-	let track = tracks[0];
-	return {
-		title: track.name,
-		artist: track.artist.name,
-		// playcount: track.playcount,
-		src: track.image[3]["#text"],
-	};
+	console.log(JSON.stringify(json));
+	const track = json.recenttracks.track[0];
+	return track.image[3]["#text"];
+	// return JSON.stringify({
+	//   title: track.name,
+	//   artist: track.artist["#text"],
+	//   img: track.image[3]["#text"],
+	//   album: track.album["#text"],
+	// });
 }
 
 addEventListener("fetch", (event) => {
 	return event.respondWith(httpRequest(event.request.url));
 });
-
 async function httpRequest(url) {
 	const entries = url.split("/");
-
-	return new Response(await getAlbums(entries[entries.length - 1]));
+	if (entries.length !== 4) {
+		return new Response("<h1>Please add a username to the url</h1>");
+	}
+	return Response.redirect(await getAlbums(entries[entries.length - 1]), 301);
 }
